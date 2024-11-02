1c57bff24202cd3bbbc47ec6b6c22377917019f1
Eldar Dikopp <eldar.dikopp@yandex.ru>
2024-11-02 14:05:26 +0300

    v. 0.9 ready

--- tests/test_login_user.py
+++ tests/test_login_user.py
@@ -6,14 +6,14 @@ from src.request import log_user, false_user
 
 class TestLogin:
 
-    @allure.title('логин под существующим пользователем.')
+    @allure.title('Логин под существующим пользователем.')
     def test_login_success(self):
         payload = log_user()
         response = requests.post(L_USER, json=payload)
         assert response.status_code == 200
         assert response.json()["success"] == True
 
-    @allure.title('логин с неверным логином и паролем.')
+    @allure.title('Логин с неверным логином и паролем.')
     def test_login_false(self):
         payload = false_user()
         response = requests.post(L_USER, json=payload)
